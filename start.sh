f3eb09f6b9afd8709654f3f87ce1bcf6128f1895
Esteban Escobar <esteban.e.gomez@gmail.com>
2023-11-11 20:59:11 +0000

    art download

--- start.sh
+++ start.sh
@@ -16,32 +16,42 @@ PWTOOLS_REPO_URL="https://api.github.com/repos/aws-powertools/powertools-lambda-
 main() {
     echo "Enter project name..."
     read -r PROJECT_NAME
-    if [ -z $PROJECT_NAME];then
+    if [ -z $PROJECT_NAME ];then
         echo "PROJECT_NAME cannot be empty..."
+    elif [ -d $PROJECT_NAME ];then
+        echo "./$PROJECT_NAME project already exists." && exit 1
     fi
 
-    echo "Enter project description...[default project description]"
+    echo "Enter project description..."
     read -r PROJECT_DESCRIPTION
 
-    if [ -z $PROJECT_DESCRIPTION];then
-        echo "PROJECT_DESCRIPTION cannot be empty..."
+    if [ -z "$PROJECT_DESCRIPTION" ];then
+        echo "PROJECT_DESCRIPTION cannot be empty..." && exit 1
     fi
+
+    mkdir "$PROJECT_NAME" || exit 1
+    cd "$PROJECT_NAME" || exit 1
+
+    download
 }
 
 download(){
     echo "Fetching latest version of TS Powertools project..."
-    $LATEST_PWTOOLS_RELEASE=$(curl "${PWTOOLS_REPO_URL}/releases/latest" | jq -r .id)
-    echo "Downloading latest version $LATEST_PWTOOLS_RELEASE"
-    
-    
+
+    LATEST_PWTOOLS_RELEASE_OBJ=$(curl "${PWTOOLS_REPO_URL}/releases/latest")
+    LATEST_PWTOOLS_RELEASE_ID=$(echo $LATEST_PWTOOLS_RELEASE_OBJ | jq -r .id)
+    LATEST_PWTOOLS_RELEASE_TAG_NAME=$(echo $LATEST_PWTOOLS_RELEASE_OBJ | jq -r .tag_name)
+    LATEST_PWTOOLS_RELEASE_TARBALL=$(echo $LATEST_PWTOOLS_RELEASE_OBJ | jq -r .tarball_url)
+
+    echo "Downloading latest version $LATEST_PWTOOLS_RELEASE_TAG_NAME"
+
+    mkdir -p staging && curl -L -o staging/package.tar.gz $LATEST_PWTOOLS_RELEASE_TARBALL
+    cd staging && tar -xzf package.tar.gz
+    cp -a aws-*/examples/sam/* ..
 }
 
+main
 
-mkdir "$PROJECT_NAME" || echo "$PROJECT_NAME project already exists in your directory" && exit 1;
-
-cd "$PROJECT_NAME" || exit 1
-
-npm i typescript --sa
 
 
 
